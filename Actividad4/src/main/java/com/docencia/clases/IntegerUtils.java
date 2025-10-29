b34f8193f417c1ede270e84255395bfc50157220
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-10-29 20:25:58 +0000

    clases y arrays

---
+++ Actividad4/src/main/java/com/docencia/clases/IntegerUtils.java
@@ -0,0 +1,19 @@
+package com.docencia.clases;
+
+public final class IntegerUtils {
+    private IntegerUtils() {}
+
+    public static String bin(int x) {
+        return null;
+    }
+
+
+    public static int popcount(int x) {
+         return 0;
+        }
+
+
+    public static int comparaSinSigno(int a, int b) { 
+        return 0;
+    }
+}
