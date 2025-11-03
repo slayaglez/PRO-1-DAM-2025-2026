fa9bf66be6d98325fec979ed764f163b4ec6bd45
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-11-03 21:24:33 +0000

    Ej14

--- Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
+++ Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
@@ -1,13 +1,28 @@
 package com.docencia.programacion;
-
+/**
+ * @author slayaglez
+ * @version 1.0.0.
+ */
 public class Ejercicio4 {
+    /**
+     * Funcion que calcula el area de un rectangulo
+     * @param base la base del rectangulo
+     * @param height la altura del rectangulo
+     * @return el area del rectangulo
+     */
     public static int area(int base, int height) {
-        // TODO implementar
-        return -1;
+        int area = base * height;
+        return area;
     }
 
+    /**
+     * Funcion que calcula el perimetro de un rectangulo
+     * @param base la base del rectangulo
+     * @param height la altura del rectangulo
+     * @return el perimetro del rectangulo
+     */
     public static int perimeter(int base, int height) {
-        // TODO implementar
-        return -1;
+        int perimetro = (base * 2) + (height * 2);
+        return perimetro;
     }
 }
