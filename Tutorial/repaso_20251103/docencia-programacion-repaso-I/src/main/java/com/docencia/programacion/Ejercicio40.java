6a4ed9dc0722aec72dbc381f98f2df5a5464b154
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-11-05 20:29:05 +0000

    update

--- Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio40.java
+++ Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio40.java
@@ -10,15 +10,20 @@ public class Ejercicio40 {
         board = new char[3][3];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                
+                board[i][j]= ' ';
             }
         }
         
     }
 
     public boolean placeMark(int row, int col, char player) {
-        // TODO implementar
+        
+        if(board[row][col] == ' ' && (player == 'X' || player == 'O')){
+            board[row][col] = player;
+            return true;
+        }
         return false;
+        
     }
 
     public char checkWinner() {
