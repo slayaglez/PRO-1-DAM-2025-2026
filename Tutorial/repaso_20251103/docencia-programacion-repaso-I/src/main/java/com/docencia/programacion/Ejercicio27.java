955801f7d9f4d4e7fda1ecebdc887451381aee5e
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-11-06 18:45:33 +0000

    update

--- Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio27.java
+++ Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio27.java
@@ -1,13 +1,42 @@
 package com.docencia.programacion;
-
+/**
+ * Clase que opera en 2D con matrices sumando filas y columnas
+ * @author slayaglez
+ * @version 1.0.0
+ */
 public class Ejercicio27 {
+
+    /**
+     * Funcion que suma todos los elementos de la fila de una matriz
+     * @param matrix la matriz
+     * @param rowIndex el indice de la fila que se quiera sumar
+     * @return un entero con el valor de la suma
+     */
     public static int sumRow(int[][] matrix, int rowIndex) {
-        // TODO implementar
-        return -1;
+        if(matrix == null || rowIndex < 0 || rowIndex > matrix.length) {
+            return 0;
+        }
+        int resultado=matrix[rowIndex][0];
+        for (int i = 1; i < matrix[rowIndex].length; i++) {
+            resultado += matrix[rowIndex][i];
+        }
+        return resultado;
     }
 
+    /**
+     * Funcion que suma todos loes elementos de la columna de una matriz
+     * @param matrix la matriz
+     * @param colIndex el indice de la columna que se quiera sumar
+     * @return un entero con el valor de la su
+     */
     public static int sumColumn(int[][] matrix, int colIndex) {
-        // TODO implementar
-        return -1;
+        if(matrix == null || colIndex < 0 || colIndex > matrix.length) {
+            return 0;
+        }
+        int resultado=matrix[0][colIndex];
+        for (int i = 1; i < matrix.length; i++) {
+            resultado += matrix[i][colIndex];
+        }
+        return resultado;
     }
 }
--- Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio40.java
+++ Tutorial/repaso_20251103/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio40.java
@@ -4,7 +4,7 @@ public class Ejercicio40 {
     private final char[][] board;
 
     /**
-     * 
+     * Constructor de la matriz, asigna el valor ' ' a cada casilla
      */
     public Ejercicio40() {
         board = new char[3][3];
@@ -16,6 +16,13 @@ public class Ejercicio40 {
         
     }
 
+    /**
+     * Funcion que escribe el valor de player en la casilla pasada
+     * @param row fila que se quiera escribir
+     * @param col columna que se quiera escribir
+     * @param player caracter que se quiera escribir 'X' o 'O'
+     * @return si se pudo escribir o no (true/false)
+     */
     public boolean placeMark(int row, int col, char player) {
         
         if(board[row][col] == ' ' && (player == 'X' || player == 'O')){
@@ -27,7 +34,57 @@ public class Ejercicio40 {
     }
 
     public char checkWinner() {
-        // TODO implementar
+        
+        /**
+         * ANÁLISIS
+         * Hay 8 casos de victoria
+         * 
+         * Suponiendo que:
+         *  1 | 2 | 3
+         * -----------
+         *  4 | 5 | 6
+         * -----------
+         *  7 | 8 | 9
+         * 
+         * Combinaciones ganadoras serían
+         * 123, 456, 789, 147, 258, 369, 159 y 357
+         * 
+         * Traducido al array sería:
+         * b[0][0] b[0][1] b[0][2] = 123
+         * b[1][0] b[1][1] b[1][2] = 456
+         * b[2][0] b[2][1] b[2][2] = 789
+         * b[0][0] b[1][0] b[2][0] = 147
+         * b[0][1] b[1][1] b[2][1] = 258
+         * b[0][2] b[1][2] b[2][2] = 369
+         * 
+         * Casos especiales (diagonal)
+         * b[0][0] b[1][1] b[2][2] = 159
+         * b[0][2] b[1][1] b[2][0] = 357
+         * 
+         * Si en estas casillas hay una misma marca gana dicha marca
+         * 
+         * Identifico el patrón, si la misma fila b[X][] o la misma columna b[][X] tiene marca, gana dicha marca
+         * Bucle for que recorra la matriz y un if que compare b[X][] y b[][X].
+         */
+
+        int ganador=0;
+        // Esto ubica un elemento X
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[0][j]== 'X') { //123
+                    ganador++;
+                }
+                if (board[1][j]== 'X') { //456
+                    ganador++;
+                }
+                if (board[2][j]== 'X') { //789
+                    ganador++;
+                }
+            }
+        }
+        if (ganador == 3){
+            return 'X';
+        }
         return ' ';
     }
 }
