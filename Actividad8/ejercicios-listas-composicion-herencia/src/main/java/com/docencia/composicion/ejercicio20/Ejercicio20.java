0ef6b355fe52a991d8c301a4e081317b009f63e5
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-12-01 17:49:26 +0000

    Ej20

--- Actividad8/ejercicios-listas-composicion-herencia/src/main/java/com/docencia/composicion/ejercicio20/Ejercicio20.java
+++ Actividad8/ejercicios-listas-composicion-herencia/src/main/java/com/docencia/composicion/ejercicio20/Ejercicio20.java
@@ -1,5 +1,8 @@
 package com.docencia.composicion.ejercicio20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Ejercicio 20 - ver la descripción detallada en el README.md.
  *
@@ -7,5 +10,27 @@ package com.docencia.composicion.ejercicio20;
  * de composición correspondientes al enunciado.
  */
 public class Ejercicio20 {
-    // TODO: implementar diseño de clases para el ejercicio 20
+
+    public static void main(String[] args) {
+        Mascota mascota1 = new Mascota("Loro", "Paco");
+        Mascota mascota2 = new Mascota("Perro", "Paco");
+        Mascota mascota3 = new Mascota("Perro", "Scooby");
+
+        Cita cita1 = new Cita("23/01/2026", "pico roto", mascota1);
+        Cita cita2 = new Cita("05/12/2025", "Maulla por algún motivo", mascota2);
+        Cita cita3 = new Cita("01/12/2015", "Puede hablar", mascota3);
+        Cita cita4 = new Cita("01/12/2015", "Apología al odio", mascota3);
+
+
+        List<Cita> citas = new ArrayList<>();
+        citas.add(cita1);
+        citas.add(cita2);
+        citas.add(cita3);
+        citas.add(cita4);
+        Clinica clinica = new Clinica(citas);
+
+        System.out.println(clinica.obtenerCitaPorFecha("01/12/2015"));
+    }
+
+    
 }
