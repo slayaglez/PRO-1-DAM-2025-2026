0148909a96919389786ccb2e25911f497e5c60d3
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-12-09 20:14:19 +0000

    update

--- Actividad8/ejercicios-listas-composicion-herencia/src/main/java/com/docencia/herencia/ejercicio30/Ejercicio30.java
+++ Actividad8/ejercicios-listas-composicion-herencia/src/main/java/com/docencia/herencia/ejercicio30/Ejercicio30.java
@@ -1,5 +1,12 @@
 package com.docencia.herencia.ejercicio30;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.docencia.herencia.ejercicio21.Circulo;
+import com.docencia.herencia.ejercicio21.Figura;
+import com.docencia.herencia.ejercicio21.Triangulo;
+
 /**
  * Ejercicio 30 - ver la descripción detallada en el README.md.
  *
@@ -8,5 +15,23 @@ package com.docencia.herencia.ejercicio30;
  * al enunciado.
  */
 public class Ejercicio30 {
-    // TODO: implementar jerarquía de herencia para el ejercicio 30
+    public static void main(String[] args) {
+        Figura triangulo = new Triangulo(10, 2);
+        Figura circulo1 = new Circulo(2);
+        Figura circulo2 = new Circulo(4);
+        Figura cuadrado = new Figura(5,5);
+
+        List<Figura> figuras = new ArrayList<>();
+        figuras.add(cuadrado);
+        figuras.add(triangulo);
+        figuras.add(circulo1);
+        figuras.add(circulo2);
+        
+
+        GrupoFiguras grupoFiguras = new GrupoFiguras(figuras);
+        System.out.println(grupoFiguras.areaTotal());
+        System.out.println(grupoFiguras.areaTotalCirculo());
+        System.out.println(grupoFiguras.areaTotalOtros());
+        System.out.println(grupoFiguras.areaTotalTriangulo());
+    }
 }
