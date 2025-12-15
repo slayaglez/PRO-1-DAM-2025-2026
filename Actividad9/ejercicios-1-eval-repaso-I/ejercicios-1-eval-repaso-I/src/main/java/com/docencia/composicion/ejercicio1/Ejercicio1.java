317b44dc3f0d3b5d6cd9db9a4bba851aa394eb68
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-12-15 16:09:02 +0000

    repaso

--- Actividad9/ejercicios-1-eval-repaso-I/ejercicios-1-eval-repaso-I/src/main/java/com/docencia/composicion/ejercicio1/Ejercicio1.java
+++
@@ -1,8 +0,0 @@
-package com.docencia.composicion.ejercicio1;
-
-public class Ejercicio1 {
-
-    public static void main(String[] args) {
-        System.out.println("Composición - Ejercicio 1: ejecuta los tests con mvn test");
-    }
-}
--- Actividad9/ejercicios-1-eval-repaso-I/ejercicios-1-eval-repaso-I/src/main/java/com/docencia/herencia/ejercicio1/Figura.java
+++
@@ -1,8 +0,0 @@
-package com.docencia.herencia.ejercicio1;
-
-/**
- * Figura geométrica con un método para calcular su área.
- */
-public abstract class Figura {
-    public abstract double area();
-}
