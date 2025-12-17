a6649d8f989fdf25806c3100ef7e6c77f71354a8
Sebastian Laya Gonzalez <sebastianlaya01@gmail.com>
2025-12-17 19:20:58 +0000

    array finished

--- 1erTrimestre/RepasoNavidad/src/main/java/com/docencia/arrays/ejercicio10/Ejercicio10.java
+++ 1erTrimestre/RepasoNavidad/src/main/java/com/docencia/arrays/ejercicio10/Ejercicio10.java
@@ -1,12 +1,32 @@
+/**
+ * @author slayaglez
+ * @version 1.0.0
+ * @brief Clase que sustituye los elementos negativos por cero
+ */
 package com.docencia.arrays.ejercicio10;
 
 public class Ejercicio10 {
     /**
      * Devuelve un nuevo array donde los negativos se sustituyen por 0.
+     * @param numeros array de numeros
+     * @return array con negativos sustituidos por cero
      */
     public static int[] normalizarNegativos(int[] numeros) {
-        // TODO: implementar usando un bucle for (no usar streams)
-        throw new UnsupportedOperationException("No implementado");
+        if(numeros == null || numeros.length == 0){
+            return null;
+        }
+
+        int[] resultado = new int[numeros.length];
+
+        for (int i = 0; i < numeros.length; i++) {
+            if(numeros[i] < 0){
+                resultado[i] = 0;
+            } else {
+                resultado[i] = numeros[i];
+            }    
+            
+        }
+        return resultado;
     }
 
     public static void main(String[] args) {
