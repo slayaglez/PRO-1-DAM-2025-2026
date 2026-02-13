abdcbde8d4b1d115de308d1a7c1eea5ce36c3d28
Cbas <sebastianlaya01@gmail.com>
2026-02-13 16:21:42 +0000

    fehechoho

--- 2doTrimestre/Actividad14/src/main/java/com/docencia/expresiones/ejercicio2/Ejercicio2.java
+++ 2doTrimestre/Actividad14/src/main/java/com/docencia/expresiones/ejercicio2/Ejercicio2.java
@@ -1,7 +1,11 @@
+/**
+ * @author slayaglez
+ * @version 1.0.0
+ * @brief Programa que trabaja con fechas y strings
+ */
 package com.docencia.expresiones.ejercicio2;
 
 import java.time.LocalTime;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Ejercicio2 {
@@ -10,6 +14,18 @@ public final class Ejercicio2 {
 
 
     public static LocalTime extractFirstTime(String text) {
-         return null;
+        if(text == null || text.isBlank()){
+            throw new IllegalArgumentException();
+        }
+
+        String patron = "^(2[0-4]||[0-1][0-9]):([0-5][0-9])$";
+        String[] arrayPalabras = text.split(" ");
+
+        for (String palabra : arrayPalabras) {
+            if(Pattern.matches(patron, palabra)){
+                return LocalTime.parse(palabra);
+            }
+        }
+        throw new IllegalArgumentException();
     }
 }
--- 2doTrimestre/Actividad14/src/main/java/com/docencia/fechas/ejercicio9/Ejercicio9.java
+++ 2doTrimestre/Actividad14/src/main/java/com/docencia/fechas/ejercicio9/Ejercicio9.java
@@ -1,14 +1,21 @@
+/**
+ * @author slayaglez
+ * @version 1.0.0
+ * @brief Programa que trabaja con instantes
+ */
 package com.docencia.fechas.ejercicio9;
 
-import java.time.Duration;
 import java.time.Instant;
-import java.util.Objects;
 
 public final class Ejercicio9 {
 
     private Ejercicio9() {}
 
     public static boolean canCancelWithin48h(Instant purchase, Instant now) {
-         return false;
+        if(purchase == null || now == null){
+            throw new NullPointerException();
+        }
+
+        return now.isBefore(purchase.plusSeconds(3600*48)) || now.equals(purchase.plusSeconds(3600*48));
     }
 }
